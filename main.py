9e195dbd5b5710c45d9b884eda367f8e79124b30
MarekWlazloo <130281067+MarekWlazloo@users.noreply.github.com>
2024-06-05 13:07:23 +0200

    Upload

--- main.py
+++ main.py
@@ -1,103 +1,137 @@
 import random  
 import numpy as np  
 
-#Wymiary kontenera
-W, H = 50, 50  # Okreslenie wymiarow kontenera
-container_area = W * H  # Obliczenie calkowitej powierzchni kontenera
+#wymiary kontenera
+W, H = 10, 10  
+#pole kontenera
+container_area = W * H 
+#lista pudelek (pusta)
+box_dimensions = [] 
 
-box_dimensions = []  # Inicjalizacja pustej listy na wymiary pudelek
-
-# Otwarcie pliku 'm3e.txt' do odczytu
-with open('m3e.txt', 'r') as file:
+#funkcja ktora z pliku wczytuje baze danych (pudelka o okreslonych rozmiarach)
+with open('m1a.txt', 'r') as file:
     width = None
-    # Iteracja przez kazda linie w pliku
     for line in file:
-        # Podzielenie linii na poszczegolne wartosci i usuniecie bialych znakow
         values = line.strip().split()
         for value in values:
-            dimension = int(value)  # Konwersja wartosci na liczbe calkowita
+            dimension = int(value)  
             if width is None:
-                width = dimension  # Jesli szerokosc jeszcze nie zostala ustawiona, ustaw ja
+                width = dimension  
             else:
-                height = dimension  # W przeciwnym razie ustaw wysokosc
-                box_dimensions.append((width, height))  # Dodaj pudelko do listy wymiarow pudelek
-                width = None  # Zresetuj szerokosc na kolejna iteracje
+                height = dimension 
+                box_dimensions.append((width, height)) 
+                width = None 
+#wyswietlenie wczytanych pudelek ()
+#print(box_dimensions)
 
-print(box_dimensions)  # Wydrukuj wymiary pudelek
-
-# Funkcja pomocnicza do sprawdzania, czy pudelko moze byc umieszczone na danej pozycji
+#funkcja ktora sprawdza czy dane pudelko mozna umiescic w okreslonym miejscu (x,y) w naszym kontenerze
 def can_place_box(box, position, packed_boxes):
-    w, h = box  # Szerokosc i wysokosc pudelka
-    x, y = position  # Pozycja pudelka
+    #szerokosc i wysokosc pudelka
+    w, h = box 
+    #pozycja pudelka w kontenerze (x,y)
+    x, y = position
+    #sprawdzenie czy pudelko miesci sie w kontenerze
     if x + w > W or y + h > H:
-        return False  # Pudelko wychodzi poza granice kontenera
+        #jesli warunek jest spelniony to znaczy ze pudelko sie nie miesci w granicach kontenera -- zwracamy False
+        return False 
+    #sprawdzenie w petli czy nasze pudelko nie zachodzi na inne pudelka, ktore juz sa zapakowane w packed_boxes
     for other_box, other_pos in packed_boxes:
+        #przypisanie wymiarow sprawdzanego pudelka (zapakowanego)
         other_w, other_h = other_box
+        #przypisanie pozycji sprawdzanego pudelka (zapakowanego)
         other_x, other_y = other_pos
         if not (x + w <= other_x or x >= other_x + other_w or y + h <= other_y or y >= other_y + other_h):
-            return False  # Pudelka zachodza na siebie
+            #jesli warunek nie jest spelniony to nasze pudelko nachodzi na inne pudelka -- zwaracmy False
+            return False 
     return True
 
-# Funkcja do oceny rozwiazania
+#funkcja ktora ocena dane rozmieszczenie pudelek pod katem liczby zapakowanych pudelek oraz miejsca ktore zajmuja
 def evaluate_solution(solution):
-    packed_boxes = []  # Lista umieszczonych pudelek
-    total_area = 0  # Calkowita powierzchnia zajeta przez pudelka
+    #inicjalizacja pustych pudelek oraz zajetej przez nich przestrzeni (0)
+    packed_boxes = []  
+    total_area = 0  
+    #petla ktora przechodzi przez wszystkie pudelka i ich rozmieszczenia w zmiennej solution
     for box, position in solution:
+        #sprawdzenie czy okreslone pudelko mozna zapakowac w danym miejscu
         if can_place_box(box, position, packed_boxes):
-            packed_boxes.append((box, position))  # Dodaj pudelko do listy umieszczonych pudelek
-            total_area += box[0] * box[1]  # Dodaj powierzchnie pudelka do calkowitej powierzchni
-    return len(packed_boxes), total_area  # Zwroc liczbe umieszczonych pudelek i calkowita zajeta powierzchnie
+            #jesli tak to dodajemy pudelko do zmiennej packed_boxes
+            packed_boxes.append((box, position)) 
+            #aktualizujemy calkowite pole zajmowane przez pudelka (obliczamy pole dodanego przed chwila pudelka i dodajemy do zmiennej total_area)
+            total_area += box[0] * box[1]
+    #zwracamy liczbe umieszczonych pudelek w kontenerze oraz calkowita przestrzen ktora one zajmuja -- sa to dwa parametry ktore algorytm ma za zadanie maksymalizowac
+    return len(packed_boxes), total_area
 
-# Generowanie poczatkowego losowego rozwiazania
+#generowanie poczatkowego, losowego rozwiazania
 def generate_random_solution():
-    solution = []  # Inicjalizacja pustego rozwiazania
+    #inicjalizacja pustego rozwiazania ()
+    solution = []
     for box in box_dimensions:
-        x = random.randint(0, W - box[0])  # Losowy x
-        y = random.randint(0, H - box[1])  # Losowy y
-        solution.append((box, (x, y)))  # Dodaj pudelko z losowa pozycja do rozwiazania
+        #wybranie losowej pozycji x danego pudelka
+        x = random.randint(0, W - box[0])  
+        #wybranie losowej pozycji y danego pudelka
+        y = random.randint(0, H - box[1])  
+        #dodanie pudelka do rozwiazania
+        solution.append((box, (x, y)))  
     return solution
 
-# Operator mutacji
+#operator genetyczny -- funkcja mutacji, ktora najpierw wybiera losowe pudelko a nastepnie w losowy sposob okresla jego nowa pozycje (koordynaty)
 def mutate(solution):
-    if random.random() < 0.1:  # Szansa mutacji
+    #okreslenie szansy na mutacje (domyslnie mniej niz 10%)
+    if random.random() < 0.1:
+        #losowanie indeksu pudelka, ktorego polozenie zostanie zmienione
         index = random.randint(0, len(solution) - 1)  # Losowy indeks pudelka do zmutowania
-        box = solution[index][0]  # Wybierz pudelko
-        x = random.randint(0, W - box[0])  # Nowe losowe x
-        y = random.randint(0, H - box[1])  # Nowe losowe y
+        #wybranie tego wylosowanego pudelka
+        box = solution[index][0]  
+        #wybranie nowej losowej pozycji x wylosowanego pudelka
+        x = random.randint(0, W - box[0])  
+        #wybranie nowej losowej pozycji y wylosowanego pudelka
+        y = random.randint(0, H - box[1]) 
+        #mutowanie pudelka -- podmienienie jego wspolrzednych w solution
         solution[index] = (box, (x, y))  # Zmutuj pudelko
 
-# Algorytm genetyczny
+#algorytm genetyczny -- zlozenie wszystkich funkcji w calosc
 def genetic_algorithm(population_size=100, generations=1000):
-    # Inicjalizacja populacji
-    population = [generate_random_solution() for _ in range(population_size)]  # Generowanie poczatkowej populacji losowych rozwiazań
-    best_solution = None  # Inicjalizacja zmiennej przechowujacej najlepsze rozwiazanie
-    best_fitness = (-1, -1)  # Inicjalizacja zmiennej przechowujacej najlepszy wynik fitness
-
+    #stworzenie poczatkowej populacji losowych rozwiazan
+    population = [generate_random_solution() for _ in range(population_size)] 
+    #inicjalizacja zmiennej przechowujacej najlepsze rozwiazanie
+    best_solution = None  
+    #inicjalizacja zmiennej przechowujacej najlepszy wynik dopasowania (liczba pudelek w kontenerze, calkowite pole zajmowane przez pudelka)
+    best_fitness = (-1, -1)  
+    #petla po kolejnych generacjach (pokoleniach)
     for generation in range(generations):  # Petla iterujaca po kolejnych pokoleniach
-        # Ocena fitnessu
-        fitnesses = [evaluate_solution(ind) for ind in population]  # Ocena fitnessu dla kazdego rozwiazania w populacji
+        #ocena pod katem liczby pudelek oraz calkowitego pola, kazdego mozliwego rozwiazania w populacji
+        #stworzenie tablicy fitnesses przechowujacej informacje o liczbie pudelek oraz calkowitym zajmowanym polu
+        fitnesses = [evaluate_solution(ind) for ind in population] 
 
-        # Wybierz najlepsze rozwiazanie
+        #wybranie najlepszego rozwiazania z tablicy fitness
         for ind, fitness in zip(population, fitnesses):  # Iteracja po populacji i odpowiadajacym jej wynikom fitness
-            if fitness[0] >= best_fitness[0]:  # Jesli liczba pudelek jest wieksza lub rowna najlepszemu dotychczasowemu wynikowi
-                if fitness[1] >= best_fitness[1]:  # Jesli zajeta powierzchnia jest wieksza lub rowna najlepszemu dotychczasowemu wynikowi
-                    best_solution = ind  # Aktualizacja najlepszego rozwiazania
-                    best_fitness = fitness  # Aktualizacja najlepszego wyniku fitness
+            #jesli nowa liczba pudelek jest wieksza lub rowna najlepszemu dotychczasowemu wynikowi
+            if fitness[0] >= best_fitness[0]:
+                #jesli nowa zajeta powierzchnia jest wieksza lub rowna najlepszemu dotychczasowemu wynikowi
+                if fitness[1] >= best_fitness[1]:
+                    #to znaczy ze znalezlismy lepsze rozwiazanie wiec je aktualizujemy
+                    best_solution = ind 
+                    #aktualizacja najlepszego wyniku (liczba pudelek, zajmowany obszar)
+                    best_fitness = fitness 
 
-        # Selekcja
-        selected_parents = random.choices(population, weights=[f[0] + f[1] for f in fitnesses], k=population_size)  # Wybor rodzicow na podstawie ich wynikow fitness
-
-        # Mutacja i budowanie nowej populacji
-        new_population = []  # Inicjalizacja nowej populacji
-        for parent in selected_parents:  # Iteracja po wybranych rodzicach
-            mutated_child = parent[:]  # Kopia rodzica
-            mutate(mutated_child)  # Mutacja dziecka
-            new_population.append(mutated_child)  # Dodanie zmutowanego dziecka do nowej populacji
-
-        population = new_population  # Aktualizacja populacji na nowa populacje
-
-        print(f'Generation {generation}: Best Fitness {best_fitness}')  # Wydruk informacji o aktualnym pokoleniu i najlepszym wyniku fitness
-
-    return best_solution, best_fitness  # Zwrocenie najlepszego rozwiazania i jego wyniku fitness
+        #operator selekcji -- wybieranie rodzicow, na podstawie ich wynikow fitness (do mutacji)
+        selected_parents = random.choices(population, weights=[f[0] + f[1] for f in fitnesses], k=population_size)  
+        #budowanie nowej populacji przy wykorzystaniu mutacji, inicjalizacja nowej populacji
+        new_population = []
+        #petla po wybranych w procesie selekcji rodzicach
+        for parent in selected_parents:
+            #tworzenie kopii rodzica
+            mutated_child = parent[:] 
+            #mutowanie dziecka (kopii rodzica)
+            mutate(mutated_child)  
+            #dodanie zmutowanego dziecka do nowej populacji
+            new_population.append(mutated_child) 
+        #aktuaclizacja pupulacji
+        population = new_population 
+        #wyswietlenie informacji o aktualnej generacji i najelpszym uzyskanym dopasowaniu (wyniku)
+        print(f'Generation {generation}: Best Fitness {best_fitness}')
+    return best_solution, best_fitness
+best_solution, best_fitness = genetic_algorithm()
+print("Best fitness: ",best_fitness)
 
 
